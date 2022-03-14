8b12b7b3208055bb4287b4b2b0d45c301738eb16
Oleksii Filonenko <brightone@protonmail.com>
2022-03-14 15:37:18 +0200

    Add CRI EMP rifle

---
+++ CS/Parts/JHG_EMPOnHit.cs
@@ -0,0 +1,44 @@
+using System;
+using XRL.Rules;
+using XRL.World.Effects;
+
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class JHG_EMPOnHit : IActivePart
+    {
+        public string Damage;
+        public string Duration;
+
+        public JHG_EMPOnHit()
+        {
+            base.Name = "JHG_EMPOnHit";
+            base.WorksOnEquipper = true;
+        }
+
+        public override void Register(GameObject Object)
+        {
+            Object.RegisterPartEvent(this, "WeaponMissileWeaponHit");
+            base.Register(Object);
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == "WeaponMissileWeaponHit")
+            {
+                GameObject attacker = E.GetGameObjectParameter("Attacker");
+                if (IsObjectActivePartSubject(attacker))
+                {
+                    GameObject defender = E.GetGameObjectParameter("Defender");
+                    if (defender.HasPart("Metal"))
+                    {
+                        // TODO: Discharge electric damage maybe?
+                        defender.ForceApplyEffect(new ElectromagneticPulsed(Stat.Roll(Duration)));
+                    }
+                }
+            }
+            return base.FireEvent(E);
+        }
+    }
+}
+
