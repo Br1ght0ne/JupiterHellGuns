755682d9077b7ffdcf818e876d834c20ac83c401
Oleksii Filonenko <brightone@protonmail.com>
2022-03-14 20:16:36 +0200

    Add 9mm frag shotgun

---
+++ CS/Parts/JHG_Shredder.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class JHG_Shredder : IActivePart
+    {
+        public string Damage = "1d2+1";
+
+        public JHG_Shredder()
+        {
+            base.Name = "JHG_Shredder";
+            base.WorksOnEquipper = true;
+        }
+
+        public override void Register(GameObject Object)
+        {
+            Object.RegisterPartEvent(this, "WeaponMissileWeaponHit");
+            base.Register(Object);
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == "WeaponMissileWeaponHit")
+            {
+                GameObject attacker = E.GetGameObjectParameter("Attacker");
+                if (IsObjectActivePartSubject(attacker))
+                {
+                    GameObject defender = E.GetGameObjectParameter("Defender");
+                    if (defender.HasEffect("Bleeding"))
+                    {
+                        E.SetParameter("Critical", 1);
+                    }
+                    GameObject.validate(ref attacker);
+                    defender.ApplyEffect(new Effects.Bleeding(Damage, Owner: attacker), attacker);
+                }
+            }
+            return base.FireEvent(E);
+        }
+    }
+}
+
